dd6006629a74995d375a5bca035e6655df203faf
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-29 09:25:14 +1300

    javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+// Dom selector
+
+const cardsContainer = document.getElementById('cards-container')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+const currentEl = document.getElementById('current')
+const showEl = document.getElementById('show')
+const hideEl = document.getElementById('hide')
+const questionEl = document.getElementById('question')
+const answerEl = document.getElementById('answer')
+const addQuestionBtn = document.getElementById('add-question')
+const clearBtn = document.getElementById('clear')
+const addContainer = document.getElementById('add-container')
+
+//Keep track of current card
+
