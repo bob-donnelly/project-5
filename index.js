151873caadd642577470d4b9d65b4cd1082f6a50
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-10-29 09:31:45 +1300

    javascript

--- index.js
+++ index.js
@@ -12,5 +12,27 @@ const addQuestionBtn = document.getElementById('add-question')
 const clearBtn = document.getElementById('clear')
 const addContainer = document.getElementById('add-container')
 
-//Keep track of current card
+// Keep track of current card
 
+let currentActiveCard = 0 // Which card to show
+
+// Store cards on DOM
+
+const cardEl = [] // Store DOM cards in a arrayof elements
+
+// Local storage of card data
+
+const cards = getCardsData()
+
+//Create all cards
+
+function createCards () {
+    cardsData.forEach((data, index) => createCard(data, index)) // loop through data and creating a array of cards
+}
+
+// Create a single card in the DOM
+
+function createCard() {
+    const card = document.createElement('div')
+    
+}
